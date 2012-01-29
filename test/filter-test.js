fbaf23dc03be0c275d35a2eee87fb4dbcf47b918
Craig Condon <craig@spiceapps.com>
2012-01-29 13:01:19 -0600

    added filter function

--- test/filter-test.js
+++ test/filter-test.js
@@ -4,39 +4,27 @@ assert = require('assert'),
 flatten = require('./flattenCollections');
 
 
-vows.describe('Implicit Routes').addBatch({
+vows.describe('Finding routes').addBatch({
 	
-	'An explicit collection of routes': {
+	'A mixed collection': {
 		
 		topic: function() {
 			var collection = require('./test-helper').collection({
-				
-				'-method a/**':1,
-				'a': 1,
-				'-method a': 1,//should NOT get added
-				'-method=DELETE  a': 1,
-
-				'-method=GET    a -> a/b': 1,
-				'-method=DELETE a -> a/b': 1,
-
-				'-method=GET    a -> a/b -> a/b/c': 1,
-				'-method=DELETE a -> a/b -> a/b/c': 1,
+				'-hook test': 1,
+				'-method=GET test2': 1,
+				'-hook=test test3': 1,
+				'-method=POST test3': 2
 			});
 
 			return collection;
 		},
 
-		'-method=DELETE a length = 1': function(topic) {
-			assert.equal(flatten(topic.get('a', { tags: { method: 'DELETE' }})).length, 2); 
+		'has 1 hook': function(topic) {
+			assert.equal(topic.find({ tags: { hook: true }}).length, 2);
 		},
 
-		'-method=DELETE a/b length = 3': function(topic) {
-			assert.equal(flatten(topic.get('a/b', { tags: { method: 'DELETE' }})).length, 3); //a without DELETE is also added 
-		},
-
-		'-method=DELETE a/bc length = 5': function(topic) {
-
-			assert.equal(flatten(topic.get('a/b/c', {tags: { method: 'DELETE' }})).length, 5);
-		},
+		'has 1 -method=GET': function(topic) {
+			assert.equal(topic.find({ tags: { method: 'GET'}}).length, 1);
+		}
 	}
 }).export(module);
