62095cdfb265be75d87cec0a848accdbd8d34e94
Craig Condon <craig@spiceapps.com>
2012-02-16 19:57:31 -0600

    added more tests

---
+++ test/extends-test.js
@@ -0,0 +1,31 @@
+var vows = require('vows'),
+assert = require('assert'),
+flatten = require('./flattenCollections'),
+crema = require('crema')
+
+
+vows.describe('Extends Routes').addBatch({
+	
+	'An explicit collection of routes': {
+		
+		topic: function() {
+
+			var collection = require('./test-helper').collection({
+				'a':1,
+				'a/*':1
+			});
+
+			collection.add(crema('a/*').pop(), 1);
+			collection.add(crema('a/*').pop(), 1);
+			collection.add(crema('a/*').pop(), 1);
+			collection.add(crema('a/*').pop(), 1);
+
+			return collection;
+		},
+
+		'a length = 3': function(topic) {
+			assert.equal(flatten(topic.get('a')).length, 6); 
+		}
+	}
+
+}).export(module);
